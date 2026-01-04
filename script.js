52a5eae92a940f0d96fd6df74d09a6a05a284478
Naveed-Ullah <naveedsalarzian@gmail.com>
2026-01-04 19:59:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("img").src="of.png";
+document.getElementById("text").innerHTML = "OF";
+document.getElementById("of").style.backgroundColor = "rgb(245,188,0)";
+    document.getElementById("of").style.color = "white";
+
+function btnOn(){
+    document.getElementById("img").src="on.png";
+    document.getElementById("text").innerHTML = "ON";
+    document.getElementById("on").style.backgroundColor = "rgb(245,188,0)";
+    document.getElementById("on").style.color = "white";
+
+
+    document.getElementById("of").style.backgroundColor = "white";
+    document.getElementById("of").style.color = "black";
+}
+
+function btnOf(){
+    document.getElementById("img").src="of.png";
+    document.getElementById("text").innerHTML = "OF";
+    document.getElementById("of").style.backgroundColor = "rgb(245,188,0)";
+    document.getElementById("of").style.color = "white";
+
+    document.getElementById("on").style.backgroundColor = "white";
+    document.getElementById("on").style.color = "black";
+}
